052671ddcd2c3770eb378f42b381f34e5d594565
Omid <125695665+omidjb@users.noreply.github.com>
2023-12-13 14:50:39 +0330

    Add files via upload
    this source code of C++ program will count the number of characters in a string
    until the user press enter key

---
+++ String Characters Counter.cpp
@@ -0,0 +1,15 @@
+// Counter of characters in a string
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ch; // to store the characters
+	int i; // for counter
+	
+	// It receives the characters until the user presses enter
+	for(i=0; ch= cin.get() != char(10); i++);
+	cout<<i;
+	
+	return 0;
+}
