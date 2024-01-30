5b06b83bdee353395b798e23c6314881b92d7a76
Omid <125695665+omidjb@users.noreply.github.com>
2024-01-30 17:21:22 +0330

    all integer types range
    all integer types range

---
+++ all integer types range.cpp
@@ -0,0 +1,33 @@
+// all integer types range
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	cout<<"short int min range:"<<endl;
+	cout<<SHRT_MIN<<endl<<endl;
+	
+	cout<<"short int max range:"<<endl;
+	cout<<SHRT_MAX<<endl<<endl;
+	
+	cout<<"int min range:"<<endl;
+	cout<<INT_MIN<<endl<<endl;
+	
+	cout<<"int max range:"<<endl;
+	cout<<INT_MAX<<endl<<endl;
+	
+	cout<<"long int min range:"<<endl;
+	cout<<LONG_MIN<<endl<<endl;
+	
+	cout<<"long int max range:"<<endl;
+	cout<<LONG_MAX<<endl<<endl;
+	
+	cout<<"long long int min range:"<<endl;
+	cout<<LONG_LONG_MIN<<endl<<endl;
+	
+	cout<<"long long int max range:"<<endl;
+	cout<<LONG_LONG_MAX<<endl<<endl;
+	
+	return 0;
+}
